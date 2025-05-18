d2eb98f30cdba989dcaea35eb702254936909dde
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-05-18 15:30:59 +0530

    Variables

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args){
+        int num1 = 23;
+        int num2 = 56;
+        int num3 =89;
+        short num4 = 34; // var 4
+        long num5 = 67l; // var 5
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+    }
+}
