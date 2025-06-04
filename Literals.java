e97d7005bdadd55698831ebe2d9ebe797329abc6
nakulsharma02 <nakulsharmavats123@gmail.com>
2025-06-04 22:40:37 +0530

    Literals in Java

---
+++ Literals.java
@@ -0,0 +1,11 @@
+public class Literals {
+    //Literals are any no or string or anything whic you have inserted in a variable;
+    public static void main(String[]args){
+        double num1 = 30;
+        char c1 = 'a';
+        String s1 = "Hello User";
+        byte b1 = 0b100;
+        long l1 = 345l;
+        System.out.print(num1+" "+c1+" "+s1+" "+b1+" "+l1);
+    }
+}
